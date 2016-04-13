538aac0534d7fe69159d607984ee9b51acd37f0a
Sigurd Fosseng <sigurd@fosseng.net>
2016-04-13 18:20:27 +0200

    noop factory
    fixes #13

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 import test from 'ava';
+import fnfactory from './factory';
 import fn from './';
 
 const EXAMPLE_NOOP_FUNCTION_3 = 'function noop() {}';
 
-test(t => {
+test('noop does nothing', t => {
 	t.is(typeof fn, 'function');
 	t.is(fn(), undefined);
 
@@ -15,4 +16,10 @@ test(t => {
 	t.is(fn.toString, Function.prototype.toString);
 	// Ensure the preciseness of the no operation.
 	t.is(fn.toString(), EXAMPLE_NOOP_FUNCTION_3);
+});
+
+test('noop factory produces noop functions', t => {
+	t.is(typeof fnfactory(), 'function');
+	t.is(fnfactory()(), undefined);
+	t.not(fnfactory(), fnfactory());
 });
