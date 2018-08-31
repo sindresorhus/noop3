29cae1b075f07c889ed74a3a6edc4404cbcf131d
Sindre Sorhus <sindresorhus@gmail.com>
2018-09-01 03:10:22 +0700

    Require Node.js 6

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 import test from 'ava';
-import fnfactory from './factory';
-import fn from '.';
+import noopFactory from './factory';
+import noop from '.';
 
 const EXAMPLE_NOOP_FUNCTION_3 = '() => {}';
 
 test('noop does nothing', t => {
-	t.is(typeof fn, 'function');
-	t.is(fn(), undefined);
+	t.is(typeof noop, 'function');
+	t.is(noop(), undefined);
 
 	/**
 	 * Check that function actually does nothing, for everyone.
 	 * https://github.com/sindresorhus/noop3/issues/10
 	 */
 	// Check that noop function is a function that functions like a function.
-	t.is(fn.toString, Function.prototype.toString);
+	t.is(noop.toString, Function.prototype.toString);
 	// Ensure the preciseness of the no operation.
-	t.is(fn.toString(), EXAMPLE_NOOP_FUNCTION_3);
+	t.is(noop.toString(), EXAMPLE_NOOP_FUNCTION_3);
 });
 
 test('noop factory produces noop functions', t => {
-	t.is(typeof fnfactory(), 'function');
-	t.is(fnfactory()(), undefined);
-	t.not(fnfactory(), fnfactory());
+	t.is(typeof noopFactory(), 'function');
+	t.is(noopFactory()(), undefined);
+	t.not(noopFactory(), noopFactory());
 });
